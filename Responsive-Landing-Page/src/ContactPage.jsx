0d761fdf9f12ae6a411b00e17e2c5ba5cef3d777
Mohamed <mohamedraffeek2001@gmail.com>
2024-02-06 14:09:40 +0200

    Scroll fix
    The pages scroll back to initial position on navigation

--- Responsive-Landing-Page/src/ContactPage.jsx
+++ Responsive-Landing-Page/src/ContactPage.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
 import NavigationMenu from './NavigationMenu';
 import PageBody from './PageBody';
 
 
 const ContactPage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <>
             <NavigationMenu activaPage={'contact'} />
--- Responsive-Landing-Page/src/MediaPage.jsx
+++ Responsive-Landing-Page/src/MediaPage.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
 import NavigationMenu from './NavigationMenu';
 import PageBody from './PageBody';
 
 
 const MediaPage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <>
             <NavigationMenu activaPage={'media'} />
